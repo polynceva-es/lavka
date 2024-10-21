8e7237c5b780439644c8cdee43be57581b0579ad
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2024-10-21 09:47:46 +0300

    first try mobile

--- index.js
+++ index.js
@@ -14,18 +14,53 @@ const products = [
 
 const contentSection = document.querySelector(".content");
 const button = contentSection.querySelector(".content__button");
-const cabinet = contentSection.querySelector(".content__cabinet");
+const cabinet = contentSection.querySelector(".content__cabinet"); //шкаф
+const cart = contentSection.querySelector(".content__cart"); //корзина
+const productsList = cabinet.querySelectorAll(".content__product");
 
 const fillCabinet = (array) => {
     array.map((item) => {
     cabinet.insertAdjacentHTML(
       "beforeend",
-      `<li draggable="true" id=${item.name} key=${item.name}><img src=${item.src} alt=${item.name}/></li>`
+      `<li class="content__product" id=${item.name} key=${item.name}><img src=${item.src} alt=${item.name}/></li>`
     );
   });
 };
-
 fillCabinet(products);
+
+    // find the element that you want to drag.
+    var wine = document.getElementById('wine');
+    
+    /* listen to the touchMove event,
+    every time it fires, grab the location
+    of touch and assign it to box */
+    
+    wine.addEventListener('touchmove', function(e) {
+      // grab the location of touch
+      var touchLocation = e.targetTouches[0];
+      
+      // assign box new coordinates based on the touch.
+      wine.style.left = touchLocation.pageX + 'px';
+      wine.style.top = touchLocation.pageY + 'px';
+    })
+    
+    /* record the position of the touch
+    when released using touchend event.
+    This will be the drop position. */
+    
+    wine.addEventListener('touchend', function(e) {
+      // current box position.
+      var x = parseInt(wine.style.left);
+      var y = parseInt(wine.style.top);
+    })
+    
+
+
+
+
+
+
+
 
 button.addEventListener("click", () =>
   location.assign("https://lavka.yandex.ru/")
